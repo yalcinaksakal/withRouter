3b79a414438b326dcbf6155559bce0888b4c7ff0
yalcinaksakal <yalcin.aksakal.tr@gmail.com>
2021-04-30 17:19:45 +0300

    reactRouter

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Route } from "react-router";
+import Products from "./components/Products";
+import Welcome from "./components/welcome";
+
+function App() {
+  return (
+    <div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
